e9862f2c5c6779bc62b22332d4925afed6843cbc
Diego Vitoriano <caligotori@gmail.com>
2024-04-06 12:11:11 -0300

    chore: Improve format part i.

---
+++ src/magyarul/core.py
@@ -0,0 +1,446 @@
+"""
+# Grammar
+## Vowels &
+### Front Vowels
+* Tongue in the front of the mouth.
+
+| Rövid | Sosszú | Difference           |
+|-------|--------|----------------------|
+| e     | é      | Pronunciation/Length |
+| i     | í      | Length               |
+| ö     | ő      | Length               |
+| ü     | ű      | Length               |
+
+### Back Vowels
+* Tongue in the back of the mouth.
+
+| Rövid | Sosszú | Difference           |
+|-------|--------|----------------------|
+| a     | á      | Pronunciation/Length |
+| o     | ó      | Length               |
+| u     | ú      | Length               |
+
+### Vowel Harmony
+* Back vowel words like áuto and tud take the back vowel endings with a few exceptions.
+* Front vowel words like fekete and beszél take the front vowel endings.
+* Most [endings](#endings) have front and back vowel harmony with a few exceptions.
+
+## Consonants &
+*
+
+| Rövid | Difference |
+|-------|------------|
+| c     |            |
+| cs    |            |
+| dz    |            |
+| dzs   |            |
+| gy    |            |
+| j/ly  |            |
+| ny    |            |
+| r     |            |
+| s     |            |
+| sz    |            |
+| ty    |            |
+| zs    |            |
+
+## Verbs &
+### Present: Lenni
+* Means to be (the famous verb to be all known as tu biu :-D).
+* Is an irregular verb.
+* Van/vannak are omitted when:
+    * Describing people
+    * Giving their jobs or nationalities.
+
+|     | Singular    | Meaning   |   | Plural         | Meaning  | Context  |
+|-----|-------------|-----------|---|----------------|----------|----------|
+| 1st | (Én) vagyok | I am      |   | (Mi) vagyunk   | We are   |          |
+| 2nd | (Te) vagy   | You are   |   | (Ti) vagytok   | You are  | Familiar |
+| 2nd | (Maga) van  | You are   |   | (Maguk) vannak | You are  | Formal   |
+| 2nd | (Ön) van    | You are   |   | (Önök) vannak  | You are  | Official |
+| 3rd | (Ő) van     | He/She is |   | (Ők) vannak    | They are |          |
+
+### Present: Megy
+* Means to go.
+* Is an irregular verb.
+
+|     | Singular    | Meaning     |   | Plural         | Meaning | Context  |
+|-----|-------------|-------------|---|----------------|---------|----------|
+| 1st | (Én) megyek | I go        |   | (Mi) megyünk   | We go   |          |
+| 2nd | (Te) mész   | You go      |   | (Ti) mentek    | You go  | Familiar |
+| 2nd | (Maga) megy | You go      |   | (Maguk) mennek | You go  | Formal   |
+| 2nd | (Ön) megy   | You go      |   | (Önök) mennek  | You go  | Official |
+| 3rd | (Ő) megy    | He/She goes |   | (Ők) mennek    | They go |          |
+
+### Lesz
+* Irregular verb will be
+    Singular                                                        Context
+    Én leszek              I will be
+    Te leszel              You will be                              Familiar
+    Maga lesz              You will be                              Formal
+    Ön lesz                You will be                              Official
+    Ő lesz                 He,she will be
+
+    Plural
+    Mi leszünk             We will be
+    Ti lesztek             You will be                              Familiar
+    Maguk lesznek          You will be                              Formal
+    Önök lesznek           You will be                              Official
+    Ők lesznek             They will be
+
+### Present tense: standard verbs with singular forms
+    Back vowel verbs                                        Context
+    (Én) tudok            I know
+    (Te) tudsz            You know                          Familiar
+    (Maga) tud            You know                          Formal
+    (Ön) tud              You know                          Formal
+    (Ő) tud               He/she knowns
+
+    Front vowel verbs                                       Context
+    (Én) beszélek         I speak
+    (Te) beszélsz         You speak                          Familiar
+    (Maga) beszél         You speak                          Formal
+    (Ön) beszél           You speak                          Formal
+    (Ő) beszél            He/she speaks
+
+### Present tense: standard verbs with plural forms
+    Back vowel verbs                                        Context
+    (Mi) tudokunk        We know
+    (Ti) tudok            You know                          Familiar
+    (Maguk) tudnak        You know                          Formal
+    (Önök) tudnak         You know                          Formal
+    (Ők) tudnak           They know
+
+    Front vowel verbs                                       Context
+    (Mi) beszélünk        We speak
+    (Ti) beszéltek        You speak                          Familiar
+    (Maguk) beszélnek     You speak                          Formal
+    (Önök) beszélnek      You speak                          Formal
+    (Ők) beszélnek        They speak
+
+### Conjugation (Past): Lenni
+* Standad verb
+    Én      voltam      I was
+    Te      voltál      You were            Familiar
+    Maga    volt        You were            Formal
+    Ön      volt        You were            Official
+    Ő       volt        He/She were
+
+    Mi      voltunk     We were
+    Ti      voltanok    You were            Familiar
+    Magük   voltak      You were            Formal
+    Önök    voltak      You were            Official
+    Ők      voltak      They were
+
+### Ül (sit)
+* Standard front vowel verb
+* Has differente conjugation from front vowel verbs.
+* HINT: This is due to the ü sound.
+    Én      ülök        I sit
+    Te      ülsz        You sit                 Familiar
+    Maga    ül          You sit                 Formal
+    Ön      ül          You sit                 Official
+    Ő       ül          He/She sits
+
+    Mi      ülünk       We sit
+    Ti      ültök       You sit                 Familiar
+    Magük   ülnek       You sit                 Formal
+    Önök    ülnek       You sit                 Official
+    Ők      ülnek       They sit
+
+### Jön (come)
+* Irregular verb.
+    Én      jövök       I come
+    Te      jössz       You come                 Familiar
+    Maga    jön         You come                 Formal
+    Ön      jön         You come                 Official
+    Ő       jön         He/She comes
+
+    Mi      jövünk      We sit
+    Ti      jöttök      You sit                  Familiar
+    Magük   jönnek      You sit                  Formal
+    Önök    jönnek      You sit                  Official
+    Ők      jönnek      They sit
+
+# Iszik (drink)
+* Irregular verb.
+    Én      iszom       I drink
+    Te      iszol       You drink                Familiar
+    Maga    iszik       You drink                Formal
+    Ön      iszik       You drink                Official
+    Ő       iszik       He/She drinks
+
+    Mi      iszunk      We drink
+    Ti      isztok      You drink                Familiar
+    Magük   isznak      You drink                Formal
+    Önök    isznak      You drink                Official
+    Ők      isznak      They drink
+
+# Transitive and Intransitive verbs &
+* Iszik can also take an object. e.g. Sayin sört iszik.
+* Verbs that can take an object are called transitive. e.g. Kér: Kérek egy sört.
+* Verbs that can never take an object are called intransitive. Megy: Moziba megyek.
+
+
+## Endings
+### -t ending
+* On front vowel words the ö linking vowel is necessary when the final word is ö, ő, ü, or ű.
+* HINT: Hence why front vowel words can take either an e or ö.
+
+### -T ending
+* The object of hungarian sentence is marked by -t.
+* This is called the object or accusative ending.
+* Certain verbs requires an object and therefore a -t ending. e.g kér, köszönöm, etc.
+* If the word ends with a vowel it follows it. e.g mi - mit, fekete - feketét, kávé - kávét, etc
+* If the words ends in a consonant a linking vowel is added based on it being front or back word.
+* Back vowels: mostly o, sometimes a.
+* Front vowels: usually e, sometime ö.
+* Some consonant words might not need a linking vowerl. e.g. sör - sört, asztal - asztalt, etc.
+
+e.g. Étlap                                              Étlapot
+     Ház                                                Házat
+     Pörkölt                                            Pörkölet
+     Gyümölcs                                           Gyümölcsöt
+
+### -ba and be ending
+* They mean to and into
+* Back vowel words use -ba. Final a turns into á.
+* Front vowel words use -be. Final e turns into é.
+* Used after all foreign and some HUngaria places.
+* User enclosed places. e.g. Mozi, színház, opera, bank, uszoda, hotel, etc.
+* Words like Magyarország takes -ra and Budapest takes -re endings.
+* e.g. Étterembenbe
+
+### -ban and -ben ending
+* Its equivallent to in or at.
+* Back vowel words take -ban.
+* Front vowel words take -ebn.
+* The final a changes to á.
+* The final e changes to é.
+* They are used with the same words as -ba/be ending.
+Standard
+    Mi?         Hova/Hová?          Hol?
+    London      Londonba            Londonban
+    Firenze     Firenzébe           Firenzében
+    Mozi        Moziba              Moziban
+    Színház     Színházba           Színházban
+    Opera       Operába             Operában
+    Bank        Bankba              Bankban
+    Uszoda      Uszodába            Uszodában
+    Hotel       Hotelba             Hotelban
+    Utca        Utcába              Utcábab
+    Kerület     Kerületbe           Kerületben
+Exception
+    Mi?              Hova/Hová?             Hol?
+    Budapeste        Budapestre             Budapesten
+    Magyarország     Magyarországra         Magyarországon
+    Repülőtér        Repülőtérre            Repülőtéren
+    Tér              Térre                  Téren
+    Út               Útra                   Úton
+    Körút            Körútra                Körúton
+
+## Personal Pronouns &
+|     | Singular | Meaning |   | Plural | Meaning | Context  |
+|-----|----------|---------|---|--------|---------|----------|
+| 1st | Én       | I       |   | Mi     | We      |          |
+| 2nd | Te       | You     |   | Ti     | You     | Familiar |
+| 2nd | Maga     | You     |   | Maguk  | You     | Formal   |
+| 2nd | Ön       | You     |   | Önök   | You     | Official |
+| 3rd | Ő        | He/She  |   | Ők     | They    |          |
+
+* Except for Ő and Ők, personal pronouns are usually omitted.
+* Unless meant for emphasis or clarity.
+* Ő means He/She, but its "agenderized".
+
+# Semantics
+
+# Intonation
+Statements        Medium to falling pitch
+Questions
+    Question-word   Rise high pitch on first syllable
+    Yes-or-no       Medium pitch to a slight drop to rise high pitch on the last or penultimate syllable
+* If there are only one or two syllable in and after the focus of the question, your voice will rise on the last syllable. e.g. Eszter éhes?
+* Uf there are three or more syllables in and aftern the focus of the question, your voice will go up on the penultimate syllable and the come down again. e.g Eszter amerikai?
+
+# Quirks
+## Esz, Azt
+| Magyar | Meaning |
+|--------|---------|
+| Esz    | This    |
+| Asz    | That    |
+
+## Is
+| Magyar | Meaning            |
+|--------|--------------------|
+| Is     | Too, Also, As well |
+
+* Always comes after the word it refers to.
+* Is never stressed.
+* Is always pronounced together with the preceding word.
+
+e.g. Én is kérek sört                                   I also want beer.
+     Én sört is kérek                                   I want beer too/as well.
+
+## Pedig and, however, but, on the other hand
+| Magyar | Meaning                              |
+|--------|--------------------------------------|
+| Pedig  | And, However, But, On the other hand |
+
+* Used as a contrast in a second phrase.
+
+e.g. Először levest kérek, utána pedig pörköltet.       First I want a soup and then a stew
+     Ica kávét kér, Ági pedig teát.                     Ica wants coffee, but Ági wants tea.
+
+## A, az the
+* A for words starting with a consonant
+* Az for words starting with a vowel
+
+## Ordinal Numbers
+
+    Első            First
+    Második         Second
+    Harmadik        Third
+    Negyedik        Fourth
+    Ötödik          Fifth
+    Hatodik         Sixth
+    Hetedik         Seventh
+    Nyolcadik       Eighth
+    Kilencedik      Ninth
+    Tizedik         Tenth
+    Tizennegyedik   ELeventh
+    Tizenkettedik   Twelfth
+    Tizenharmadik   Thirteenth
+    Huszadik        Twentieth
+    Harmincadik     Thirtieth
+    Negyvenedik     Fortieth
+    Ötvenedik       Fiftieth
+    Hatvanadik      Sixtieth
+    Hetvenedik      Seventieth
+    Nyolcsvanadik   Eightieth
+    Kilencvenedik   Ninetieth
+    Századik        Hundredth
+    Ezredik         Thousandth
+
+* Ordinal numbers follow the same pattern as cardinal numbers.
+
+## Cardinal Numbers
+                                                            Context
+    Egy                                 1
+    Kettő, Két                          2                   Kettő is used standalone while két is followed by something.
+    Három                               3
+    Négy                                4
+    Öt                                  5
+    Hat                                 6
+    Hét                                 7
+    Nyolc                               8
+    Kilenc                              9
+    Tíz                                 10
+    Tizen                               Tens                Tizenegy, Tizenhat, ...
+    Húsz                                20
+    Huszon                              Twenties            Huszonegy, Huszonhat, ...
+    Harminc                             Thirties            Harmincegy, Harminchat, ...
+    Negyven                             Forties
+    Ötven                               Fifties
+    Hatvan                              Sixties
+    Hetven                              Seventies
+    Nyolcvan                            Eighties
+    Kilencven                           Nineties
+    Száz                                Hundreds
+    Ezer                                Thousands
+    Tízezer                             Ten thousands
+    Százezer                            Hundred thousands
+    Millió                              Million
+    Milliárd                            Billion
+
+* 101 is százegy, 200 is kétszás, 300 is Háromszás
+* 845 Nyolcszáznegyvenöt
+* 2600 Kétezerhatszás
+* Name of year are read in full. Jesus.
+
+## Sok, kevés, néhány
+
+    Sok                                 A lot of, many, much
+    Kevés                               A little, a few
+    Néhány                              A few
+
+* Are all followed by singular.
+
+## Mennyibe kerül
+* When answering this question put -ba or -be after the word denoting price or currency.
+* Mennyi + be
+* e.g. Ötszázhatvan forintba or Ötszázhatvanba.
+
+## Nincs, nicsen
+* Are negatives of van.
+* Both are correct.
+* Nincs is more common
+* See van and vannak
+
+## Ul and Ül
+* Back vowel words ending takes ul.
+* Front vowel words ending takes ül.
+* Use for languages
+* a and e ending words usually change to á and é. E.g. francia, franciául.
+
+## Van and Vannak
+* Can not be ommited when asking/stating how somebody is.
+
+## Egy
+* Indefinite article: a, an.
+* Has only one form.
+* Is not used when givin profession.
+
+## Van or Is
+* Used when:
+    * Asking or saying where somebody or thing is. e.g. Közel van a Blaha Lujza tér?
+    * Asking or stating how someone is. e.g. Hogy van Eszter?
+    * Using certain weather expressions. e.g. Hideg/Meleg van.
+    * Using in the sense of there is. e.g. Vat itt valahol egy posta?
+    * Asking or stating if something is available. e.g. Van gyulai kolbász?
+
+## Inner or From Here and Onnan or From There
+* Where has at least 3 ways in Hungarian. e.g. Where at? Where to? Where from?
+* Itt and ott will vary depending on which one you're answering.
+
+    Honnan?         Hol?        Hova/Hová
+    Innen           Itt         Ide
+    Onnan           Ott         Oda
+
+## Word order &
+* It can vary greatly (something similar to portuguese).
+* The golden rule is, you often want to emphasize one or a group of words.
+    This is called focus and is often followed by the verb.
+
+            Angol           vagyok              "American" I am (not another nationality)
+            Én              vagyok angol        "I" am american (not someone else)
+    Sanyi   Velencében      volt                In "Venice" Sanyi was (not somewhere else)
+            Sanyi           volt Velencében     "Sanyi" was in Venice (not someone else)
+
+* The onely twi time when the focus is not followed by the verb are:
+    * When there is no verb in the sentence. e.g. János magyar.
+    * When the focus is the verb. e.g. Kérek egy feketét.
+
+* In hungarian, stressing the word also hightlight the focus on a word.
+* HINT: The question word is always the focus of a question. Therefore when answering a question-word question, first answer the question-word and the follow the word order of the rest of the question. e.g. Hol volt Sanyi? Velencében volt Sanyi or just Velencében.
+
+## Már, még (already, yet, before)
+* Már, already, yet, before, is used in positive sentences and questions. e.g. Voltál már Budapesten?
+* Még, yet, before, is used in negative sentences and questions. e.g. Még nem voltál Budapesten?
+* HINT:
+    Még also means even on sentences like:
+        * Még te is?
+        * Igen, még én is.
+    Or still in sentences like:
+        * Jársz még angolra.
+
+## Valami (Something)
+## Valaki (Somebody)
+
+## Haza (home)
+* Has different form depending on the question.
+    Honnan?         Hol?        Hová?
+    Itthonról       Itthon      Haza
+    Otthonról       Otthon      Haza
+    Hazulról
+* Itthon and Itthonról are used when talking about your own or someone else's home and you are actually there.
+* Otthon and Otthonról are used when you are talking about your own, or someone else's home but you are not there.
+"""
