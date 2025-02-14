aaf486ed4400696eaeb6902c47985ad971943780
Diego Vitoriano <91431136+frlryr@users.noreply.github.com>
2025-02-14 11:37:28 -0300

    chore: Add new data. (#42)

---
+++ src/magyarul/b2/core.py
@@ -0,0 +1,112 @@
+"""
+# Introduction
+## Orthophony, Orthoepy
+* The Hungarian language has:
+    * 7 different sounds
+    * Each can be either long¹ or short²
+    * It produces 14 different forms of characters: vowels
+    * And 24 different articulations, marked by as many consonants
+* The vowels are:
+    * a, e, i o, ö, u, ü.
+* The protacted vowel equivalents are:
+    * á, é, í, ó, ő³, ú, ű³
+* On the equivalent and non-equivalent English articulations of compounded vowels
+    * a is articulated like French's a in fatalité
+    * á is articulated like English's a in Father
+    * e is articulated like English's a in fat. In monosyllables its softened if not
+        followed by m or a sharp consonant. Same in the words tenni, enni, menni, venni
+        and similar ones
+    * é is articulated like English's a in fate, if the assonance of e be avoided
+    * i is articulated like French's i in fine
+    * í is articulated like English's long e in deed
+    * o is articulated like French's short o. Is the short sound of ó
+    * ó is articulated like English's o in coal, loan, etc.
+    * ö is articulated like French's eu in jeudi. A very deep and unclear sound of a
+        short e
+    * ő is articulated like German's ö in öde, höle, etc. Is the protracted sound of ö
+    * u is articulated like English's u in bull
+    * ú is articulated like English's long double o in food
+    * ü is articulated like French's u in une, (je) fus, etc.
+    * ű is articulated like German's ü in übel. Is the protracted sound of ü.
+* The 24 consonants are:
+    * b, cs, cz, d, f, g, gy, h, j, k, l, ly, m, n, ny, p, r, s, sz, t, ty, v, z, zs
+    * B, Cs, Cz, D, F, G, Gy, H, J, K, L, Ly, M, N, Ny, P, R, S, Sz, T, Ty, V, Z, Zs
+* Of which 16 are simple⁴:
+    * b, d, f, g, h, j, k, l, m, n, p, r, s, t, v, z
+* And 8 are compounded⁵:
+    * cs, cz, gy, ly, ny, sz, ty, zs.
+* On the non-equivalent English articulations of compounded consonants⁶:
+    * cz is articulated like ts in English, equal to the German z
+    * gy is articulated by d followed by u as in due, duce, etc.
+    * dj is articulated by d followed by y as in English
+    * ly is articulated similarly to French's il, ille, etc. preceded by another vowel
+        as in traveil, feuille, etc.
+    * lj is articulated similarly to ly, but has been replaced by jj as in vajjon,
+        instead of valljon
+    * ny is articulated like in French's gne, and the English n with long u, or the n in
+        new
+    * ty is articulated as a sharp gy, and very nearly to the articulation of the
+        consonants t and u, as in tube.
+    * dzs is articulated like g in general.
+* The Hungarian language requires that:
+    * Each articulation and sound to be given distinctly and plainly
+    * There is no muted vowel or unarticulated consonant in any word
+    * Even in the ocurrence of two consonants be distinct. Each being articulated
+        separately
+* Here is a cheatsheet for quicker learning:
+| Spelt in English              | Spelt in Hungarian |
+|-------------------------------|--------------------|
+| Bull                          | Bul                |
+| Chamber                       | Csémber            |
+| Deed                          | Díd                |
+| Did                           | Did                |
+| Danger                        | Déndzser           |
+| Eagle                         | Ígle               |
+| Fat                           | Fet                |
+| Fate                          | Fét                |
+| German                        | Dzsermen           |
+| Loan                          | Lón                |
+| Lasting (according to Walker) | Lásztin            |
+| Moon                          | Mún                |
+| Meek                          | Mík                |
+| Newly                         | Nyúli              |
+| Oats                          | Ócz                |
+| Palm-tree                     | Pámtrí             |
+| Pulpit                        | Pulpit             |
+| Raze                          | Réz                |
+| Rule                          | Rúl                |
+| Sheep                         | Síp                |
+| Shall                         | Sell               |
+| Sea                           | Szí                |
+| Tore                          | Tór                |
+| Tulip                         | Tjúlip             |
+| Tube                          | Tjúb               |
+| Vetch                         | Vecs and Vets      |
+| Yare                          | Jér                |
+| Yellow                        | Jelló              |
+| Your                          | Júr                |
+| You                           | Ju                 |
+| Zeal                          | Zíl                |
+| Zero                          | Zíró               |
+
+NOTE:
+    ¹ By long is meant the protraction or prolongation.
+    ² By short is meant contraction, or quick pronunciation of the same sound.
+    ³ There is a older form accent: umlaut(?). This one is modern.
+    ⁴ They have the same articulations as English. Except j who equals y as a consonant,
+        s who equals to sh, g who is always hard like [g]uide, and r who always have a
+        strong trill.
+    ⁵ There are 3 articulations in English that are identical to Hungarian ones. cs who
+        equals to [ch]apter, the sz who equals to sharp [s]even, and zs who equals to
+        sei[z]ure.
+    ⁶ These require training as they're not exactly equal. And thus the most difficult
+        articulations are those of gy and ty. The latter being a sharp articulation of
+        the former.
+
+## Division of the sounds and words
+## Assimilation of sounds and articulations
+## Pronlogation of the vowels
+## Contractions
+# Compositions
+# Theory of the Language
+"""
